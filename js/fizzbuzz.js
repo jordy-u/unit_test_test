361f52ba698666b3230680b78a58d3f3b3fefdca
Jordy Ubink <jordyubink@hotmail.nl>
2020-05-15 11:15:52 +0200

    Add validation script and unit test.

---
+++ js/fizzbuzz.js
@@ -0,0 +1,14 @@
+/**
+ * @param {int} index The count number that needs to be translated to a FizzBuss string.
+ * @return {string} The FizzBuzz string.
+ */
+function getFizzBuzzStringByIndex(index) {
+    let fizzBuzzString = "";
+
+    if (index % 3 === 0) fizzBuzzString += "Fizz";
+    if (index % 5 === 0) fizzBuzzString += "Buzz";
+
+    if (fizzBuzzString === "") fizzBuzzString = index.toString();
+
+    return fizzBuzzString;
+}
