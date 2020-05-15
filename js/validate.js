2bf4688ad740eb22e9a5389c427f6eea2ad497a3
Jordy Ubink <jordyubink@hotmail.nl>
2020-05-15 10:23:26 +0200

    Add validation script and unit test.

---
+++ js/validate.js
@@ -0,0 +1,8 @@
+function checkEmpty(name) {
+    return name.trim().length === 0;
+
+}
+
+function checkContainsSpaces(name) {
+    return name.trim().indexOf(" ") !== -1;
+}
---
+++ tests/validate_test.js
@@ -0,0 +1,31 @@
+//Jasmine = Behavour Driven Design
+
+describe("The name validator", function() {
+
+    describe("Checks for empty string", function() {
+
+        it("should return true when the string is empty", function () {
+            expect(checkEmpty("     ")).toBe(true);
+
+        })
+
+        it("should return false when the string is not emtpy", function () {
+
+            expect(checkEmpty("abcd")).toBe(false);
+
+        })
+
+    })
+
+    describe("Checks for spaces between the words", function () {
+
+        it("should return true when the string has a space between the characters", function () {
+            expect(checkContainsSpaces("João the Rock")).toBe(true);
+        })
+
+        it("should return false when the string does not contain spaces between the characters", function () {
+            expect(checkContainsSpaces("João-the-Rock")).toBe(false);
+        })
+    })
+
+})
